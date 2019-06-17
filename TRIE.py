c1fc635c680cfebac254b0535a27f3d95dacd301
Nimish Verma <nimishverma@ymail.com>
2019-06-18 01:06:16 +0530

    Added Search/AutoComplete Function

--- TRIE.py
+++ TRIE.py
@@ -22,3 +22,29 @@ def constructTrie(wordList):
 		root = orignal_root
 
 	return orignal_root
+
+a = constructTrie(['cat','can','cans'])
+
+# print (a.children[2].children[0].children[13].__dict__)
+
+def inv_index(index):
+	return chr(97+index)
+
+def searchRec(node,word, results):
+    if node.end == True:
+    	results.append(word)
+    for index, n in node.children.items():
+
+        searchRec(n, word+inv_index(index), results)
+def searchInTrie(root, word):
+	for letter in word:
+		if getIndex(letter) not in root.children:
+			return -1
+		root = root.children[getIndex(letter)]
+	results = []
+	searchRec(root, word, results)
+ 			
+	return results
+
+print(searchInTrie(a,'can'))
+
